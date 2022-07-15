7779fc5447a82e01f888361b07934c1033dfbc01
Radovan Lipka <radovan.lipka@gmail.com>
2022-07-15 17:43:58 +0200

    add typescript

---
+++ week-01/day-3/two-number.ts
@@ -0,0 +1,19 @@
+'use strict'
+
+// operation numbers
+
+var added = 13 + 22;
+console.log(added);
+
+var substracted = 13 - 22;
+console.log(substracted);
+
+var multiplied = 22 * 13;
+console.log(multiplied);
+
+var divided = 22 / 13;
+console.log(divided);
+
+ var result = divided
+ console.log(result);
+ 
