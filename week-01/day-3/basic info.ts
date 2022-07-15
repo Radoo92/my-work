6f24c3128f37a114dba511e9296c04d426488237
Radovan Lipka <radovan.lipka@gmail.com>
2022-07-15 18:46:06 +0200

    add typescript

---
+++ week-01/day-3/basic info.ts
@@ -0,0 +1,9 @@
+'use strict'
+
+// basic info
+
+var a = "Radovan Lipka";
+var b = 30;
+var c = 1.86;
+
+console.log(a, b, c);
---
+++ week-01/day-3/favourite-number.ts
@@ -0,0 +1,9 @@
+'use strict'
+
+//my favourite number
+
+let a: number = 8;
+
+if (a === 8) {
+  console.log('My favorite number is: 8'); 
+}
