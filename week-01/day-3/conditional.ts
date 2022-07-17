b4cf7548114d338e6753e34d02a23e16572ca25b
Radovan Lipka <radovan.lipka@gmail.com>
2022-07-17 16:14:15 +0200

    add typescript

---
+++ week-01/day-3/conditional.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+let q: number = 24;
+let output1: number = 0;
+
+if (q ) {
+    
+}
+
+
+
+
+
+let x: number = 13;
+let output2: string = 'Sweet!';
+
+if (10 < x && x < 20) {
+    console.log(output2);
+    
+}
+
