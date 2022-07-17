d0b124384f6568b9ecacb229b0b5af6462eee7d5
Radovan Lipka <radovan.lipka@gmail.com>
2022-07-17 15:18:21 +0200

    add typescript

---
+++ week-01/day-3/cheat.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+var exam = "I won't cheat on the exam! \n";
+
+console.log(exam.repeat(100));
---
+++ week-01/day-3/seconds.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+var dayseconds: number = 24 * 60 * 60
+var curentseconds: number = 14 *60 *60 + 34 *60 + 42
+
+console.log(dayseconds - curentseconds);
