1b5eb8337db27576efc773747d82531b851ee2ab
Radovan Lipka <radovan.lipka@gmail.com>
2022-07-25 12:18:56 +0200

    swap names

---
+++ week-04/day-1/names.ts
@@ -0,0 +1,13 @@
+'use strict'
+
+{
+//swap elements
+
+    const names: string[] = ["Arthur", "Boe", "Chloe"]
+    const tmp = names[0]
+    names[0] = names[2]
+    names[2] = tmp
+
+    console.log(names);
+    
+}
