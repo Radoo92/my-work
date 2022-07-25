de78d696909acdf2e214691b829b7fd4a563311e
Radovan Lipka <radovan.lipka@gmail.com>
2022-07-25 11:44:33 +0200

    draw pyramid

--- week-01/day-3/draw.ts
+++ week-01/day-3/draw.ts
@@ -1,19 +1,15 @@
-'use strict';
+`use strict`;
+{
+const lineCount = 4;
+let star = `*`;
+let space = ` `;
 
-let lineCount: number = 4;
-let pyramid: string = ""
-
-for (let i = 0; i <= lineCount; i++) {
-  
-    for (let x = 0; x <= lineCount; i++)  {pyramid +=""}
-    
-    for (let j: number = lineCount - i; j >=0; j--) {
-    
-        
-    pyramid +=" * "
-        
-    }
-  pyramid += "\n"  
-    
+for (let i = 1; i <= lineCount; i++) {
+  for (let j: number = lineCount - i; j >= 0; j--) {
+    space += ` `;
+  }
+  console.log(space, star);
+  space = ' ';
+  star += `**`;
 }
-console.log(pyramid);
+}
