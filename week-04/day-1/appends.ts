5435f68cb27bae88884e263479f9e5f7f21c963d
Radovan Lipka <radovan.lipka@gmail.com>
2022-08-01 13:18:14 +0200

    split into multiple lines

--- week-04/day-1/appends.ts
+++ week-04/day-1/appends.ts
@@ -1,7 +1,23 @@
 'use strict';
 {
 
-    const animals = ["koal", "pand", "zebr", "anacond", "bo", "chinchill", "cobr", "gorill", "hyen", "hydr", "iguan", "impal", "pum", "tarantul", "pirahn",];
+    const animals = [
+        "koal",
+        "pand",
+        "zebr",
+        "anacond",
+        "bo",
+        "chinchill",
+        "cobr",
+        "gorill",
+        "hyen",
+        "hydr",
+        "iguan",
+        "impal",
+        "pum",
+        "tarantul",
+        "pirahn",
+    ];
 
     // -  Add all elements an `"a"` at the end
 
---
+++ week-04/day-1/matrix.ts
@@ -0,0 +1,16 @@
+'use strict';
+{
+    const matrix = [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+    ];
+
+    for (let i = 0; i < matrix.length; i++) {
+        console.log(matrix[i]);
+        
+    }
+
+}
+
