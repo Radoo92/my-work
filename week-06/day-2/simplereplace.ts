c8a08b8db7e053bed0b4eba9d7a750818ca86b7d
Radovan Lipka <radovan.lipka@gmail.com>
2022-08-07 13:45:22 +0200

    replace method

---
+++ week-06/day-2/simplereplace.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+export {};
+
+//replace "dishwasher" with "galaxy"
+
+let example: string = 'In a dishwasher far far away';
+let result = example.replace(/dishwasher/i, "galaxy")
+
+console.log(result);
+
