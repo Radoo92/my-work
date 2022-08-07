1a9dc3c0262fe81964079b4bd3e6de8e506f8d8f
Radovan Lipka <radovan.lipka@gmail.com>
2022-08-07 15:50:11 +0200

    reverse function

---
+++ week-06/day-2/reverse.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+export {};
+
+let toBeReversed: string = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
+
+function reverse() {
+    let myReverse = '';
+    for (let i = toBeReversed.length - 1; i > - 0; i--) {
+        myReverse += toBeReversed[i];
+    }
+    console.log(myReverse);
+}
+
+reverse();
+
+
+
+
+  
+
+
+
+
