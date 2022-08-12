e8d46d6ce40d98bdc91497eba5a03c4164b231ad
Radovan Lipka <radovan.lipka@gmail.com>
2022-08-12 16:45:29 +0200

    find unique numbers

--- week-04/day-2/unique.ts
+++ week-04/day-2/unique.ts
@@ -1,15 +1,17 @@
 'use strict';
 
-export{};
+export { };
 
 function findUniqueItems(array: any) {
-    var out = [];
-    for (var i = 0, len = array.length; i < len; i++)
-        if (out.indexOf(array[i]) === -1)
+    let out = [];
+    for (let i = 0, len = array.length; i < len; i++) {
+        if (out.indexOf(array[i]) === -1) {
             out.push(array[i]);
+        }
+    }
     return out;
 }
-var myArray = [1, 11, 34, 11, 52, 61, 1, 34];
-var uniqueArray = findUniqueItems(myArray);
+const myArray = [1, 11, 34, 11, 52, 61, 1, 34];
+let uniqueArray = findUniqueItems(myArray);
 
 console.log(uniqueArray);
