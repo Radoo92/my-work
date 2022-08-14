0efbc82532ef260acb696e23908a5e5671c719b1
Radovan Lipka <radovan.lipka@gmail.com>
2022-08-14 13:51:43 +0200

    colored diagonals

--- week-07/day-3/diagonals/diagonals-ts.ts
+++ week-07/day-3/diagonals/diagonals-ts.ts
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
+// If the line starts from the upper-left corner it should be green, otherwise it should be red
 ctx.strokeStyle = "#00ff00";
 ctx.beginPath();
 ctx.moveTo(0,0);
