fb6f32efe4ac5bbbd376b1ab5d4ca4f870a69a5b
Radovan Lipka <radovan.lipka@gmail.com>
2022-08-17 17:09:02 +0200

    draw three squares

--- week-07/day-3/centerbox/center-box-function-ts.ts
+++ week-07/day-3/centerbox/center-box-function-ts.ts
@@ -3,6 +3,8 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
+// Function that draws square
+// Draw a square the center
 function drawThree(size: number){
     ctx.strokeStyle = "blue"
     ctx.strokeRect(canvas.width / 2, canvas.height /2 , size, size);
@@ -11,7 +13,7 @@ function drawThree(size: number){
 let size: number = 50;
 
 for (let i: number = 1; i < 4; i++) {
-    drawThree(size);
+    drawThree(size); // Draw at least 3 squares
     size = size + 20;
 }
 
