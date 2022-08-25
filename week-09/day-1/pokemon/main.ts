11be62317991bdc6a1c4965d13b961b66ca0c0a6
Radovan Lipka <radovan.lipka@gmail.com>
2022-08-25 20:06:27 +0200

    find best pokemon

---
+++ week-09/day-1/pokemon/main.ts
@@ -0,0 +1,38 @@
+'use strict';
+
+import { Pokemon } from './Pokemon'
+
+let pokemonOfAsh: Pokemon[] = initializePokemon();
+
+// Every pokemon has a name and a type.
+// Certain types are effective against others, e.g. water is effective against fire.
+
+// Ash has a few pokemon.
+// A wild pokemon appeared!
+
+let wildPokemon: Pokemon = new Pokemon('Oddish', 'leaf', 'water');
+
+// Which pokemon should Ash use?
+
+// Implement the following `chooseEffective` function which will choose the most effective pokemon for Ash
+function chooseEffective(array: Pokemon[], wildPokemon: Pokemon) {
+    for (let i: number = 0; i < array.length; i++) {
+        if (array[i].effectiveAgainst == wildPokemon.type) {
+            return array[i];
+        }
+    }
+}
+
+let effective = chooseEffective(pokemonOfAsh, wildPokemon)
+console.log("I choose you!", effective?.name);
+
+function initializePokemon(): Pokemon[] {
+    return [
+        new Pokemon('Balbasaur', 'leaf', 'water'),
+        new Pokemon('Pikatchu', 'electric', 'water'),
+        new Pokemon('Charizard', 'fire', 'leaf'),
+        new Pokemon('Balbasaur', 'water', 'fire'),
+        new Pokemon('Kingler', 'water', 'fire')
+    ];
+
+}
