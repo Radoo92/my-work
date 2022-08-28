0a4711ec32f80053131135777d63bbd5666c637f
Radovan Lipka <radovan.lipka@gmail.com>
2022-08-28 11:09:21 +0200

    Instantiate object

--- week-09/day-3/teachers.ts
+++ week-09/day-3/teachers.ts
@@ -1,5 +1,15 @@
 'use strict'
 
+/*Create Student and Teacher classes
+Student
+learn() -> prints: the student is actually learning
+question(teacher) -> calls the teacher's giveAnswer() method
+Teacher
+teach(student) -> calls the student's learn() method
+giveAnswer() -> prints: the teacher is answering a question
+Instantiate a Student and Teacher object
+Call the student's question() method and the teacher's teach() method*/
+
 class Student {
     learn() {
         console.log("the student is actually learning");
