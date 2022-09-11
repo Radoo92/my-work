85bc55171fc8916572129b8db90cca3bfaeb482e
Radovan Lipka <radovan.lipka@gmail.com>
2022-09-11 15:12:20 +0200

    recursive function

--- week-10/day-1/numberadder.ts
+++ week-10/day-1/numberadder.ts
@@ -1,16 +1,12 @@
 'use strict';
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
 
-//Write a recursive function that takes one parameter: n and returns the sum of numbers (integers) from 1 to n. The function should return 0 for inputs less than 1.
-function recursive(n: number): number {
+function adder(n: number): number {
     if (n < 1) {
         return 0;
     }
-    else {
-        return n + recursive(n - 1);
-    }
+    return n + adder(n - 1)
 }
 
-console.log(recursive(10));
-
-export { };
+console.log(adder(5));
 
