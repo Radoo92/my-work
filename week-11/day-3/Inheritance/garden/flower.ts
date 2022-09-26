7d0889bec27ce1c6cbf878d98f1d91bd8534cf14
Radovan Lipka <radovan.lipka@gmail.com>
2022-09-26 18:50:41 +0200

    change of folders

---
+++ week-11/day-3/Inheritance/garden/flower.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+import { Plant } from "./plant";
+
+export class Flower extends Plant {
+
+    constructor(color: string) {
+        super("Flower", color);
+    }
+
+    watering(amount: number) {
+        let useAmount = amount * 0.75;
+        if (this.water < 5) {
+            super.watering(useAmount);
+            if (this.water > 4) {
+                this.isNeedWater = false;
+            }
+        }
+    }
+}
---
+++ week-11/day-3/Inheritance/garden/garden.ts
@@ -0,0 +1,33 @@
+`use strict`;
+
+import { Plant } from './plant';
+
+export class Garden {
+    plants: Plant[];
+
+    constructor() {
+        this.plants = [];
+    }
+
+    addPlant(plant: Plant) {
+        this.plants.push(plant);
+    }
+
+    print() {
+        for (let plant of this.plants) {
+            plant.printInfo();
+        }
+        console.log('');
+    }
+
+    waterPlants(amount: number) {
+        console.log(`Watering with ${amount}`)
+        let size = 0
+        for (let plant of this.plants) {
+            plant.isNeedWater ? size++ : '';
+        }
+        for (let plant of this.plants) {
+            plant.watering(amount / size);
+        }
+    }
+}
