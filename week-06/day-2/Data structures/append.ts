6954b5e10dc96273aa8945f52fee7e45b1fdb133
Radovan Lipka <radovan.lipka@gmail.com>
2022-09-28 18:31:41 +0200

    create a new function for append a

--- week-06/day-2/Data structures/append.ts
+++ week-06/day-2/Data structures/append.ts
@@ -1,11 +1,14 @@
 'use strict';
 
-let append = "a";
+// Create a function called "appendA()" that adds "a" to every string in the "animals" list
+// The parameter should be a list
+
 let animals = ["koal", "pand", "zebr", "anacond", "bo", "chinchill", "cobr", "gorill", "hyen", "hydr", "iguan", "impal", "pum", "tarantul", "pirahn"];
-let appendedAnimals = animals.map(function (element) {
-    return element + append;
-});
-console.log(appendedAnimals);
 
+function appendA(list: string[]) {
+    return list.map(x => x + "a")
+}
 
-export {};
+console.log(appendA(animals));
+
+export { };
