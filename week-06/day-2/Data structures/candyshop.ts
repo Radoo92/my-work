804a5bebf1a0d5320a4a3f11ad1791b23e27db30
Radovan Lipka <radovan.lipka@gmail.com>
2022-09-28 18:35:27 +0200

    create a function for change array elements

--- week-06/day-2/Data structures/candyshop.ts
+++ week-06/day-2/Data structures/candyshop.ts
@@ -1,16 +1,23 @@
 'use strict';
 
-function repairSweets() {
+let shopItems: any[] = ['Cupcake', 2, 'Brownie', false];
 
-    let list = ['Cupcake', 2, 'Brownie', false];
+// Accidentally we added "2" and "false" to the array
+// Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+// No, don't just remove the items :)
+// Create a function called repairSweets() which takes the list as a parameter
+// Expected output: "Cupcake", "Croissant", "Brownie", "Ice cream"
 
-    list.splice(1, 1, "Croissant");
-    list.splice(3, 1, "Ice cream");
-
-    console.log(list);
-
+function repairSweets(list: any[]) {
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] === 2) {
+            list[i] = "Croissant"
+        } else if (list[i] === false) {
+            list[i] = "Ice Cream"
+        }
+    } return list
 }
 
-repairSweets()
+console.log(repairSweets(shopItems));
 
-export {};
+export = repairSweets;
