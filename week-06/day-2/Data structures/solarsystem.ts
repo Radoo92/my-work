1e6c6db7b2e15c9e86ea2a8f8c35c58a0c9e1e4f
Radovan Lipka <radovan.lipka@gmail.com>
2022-09-28 12:05:52 +0200

    create a new function

--- week-06/day-2/Data structures/solarsystem.ts
+++ week-06/day-2/Data structures/solarsystem.ts
@@ -1,9 +1,18 @@
 'use strict';
 
-export {};
+export { };
 
-//Insert Saturn into the correct position
+// Saturn is missing from the planetList
+// Insert it into the correct position
+// Create a function called putSaturn() which has list parameter and returns the correct list.
+// bonus for using some built in methods
+
 let planetList: string[] = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Uranus', 'Neptune'];
 
 planetList.splice(5, 0, "Saturn");
-console.log(planetList.join());
+
+function putSaturn(list: string[]): any {
+    return list;
+}
+
+console.log(putSaturn(planetList));
