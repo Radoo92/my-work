d6da8acf6da4cf4941fff8952df0c2f3c7d4eccd
Radovan Lipka <radovan.lipka@gmail.com>
2022-09-28 18:45:36 +0200

    create a new function for represent expenses

--- week-06/day-2/Data structures/personalfinance.ts
+++ week-06/day-2/Data structures/personalfinance.ts
@@ -1,28 +1,24 @@
-'use strict'
 
-//Create a list
-const myExpense: number[] = [500, 1000, 1250, 175, 800, 120];
+`use strict`;
+// We are going to represent our expenses in a list containing integers.
+// Create a list with the following items.
+// 500, 1000, 1250, 175, 800 and 120
+// Create an application which solves the following problems.
+// How much did we spend?
+// Which was our greatest expense?
+// Which was our cheapest spending?
+// What was the average amount of our spendings?
 
-function getSum() {
-    let value = 0;
-    for (let i = 0; i < myExpense.length; i++) {
-        value += myExpense[i];
-    }
-    
-    //we spend
-    console.log(value);
-    
-    //greatest expense
-    console.log(Math.max(...myExpense));
-    
-    //cheapest spending
-    console.log(Math.min(...myExpense));
-    
-    //average amount
-    console.log((value / myExpense.length).toFixed(4));
-    
+let expenses = [500, 1000, 1250, 175, 800, 120];
+
+function app(arr: number[]) {
+    let sum = 0;
+    arr.map(x => sum += x);
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    let average = Math.floor(sum / arr.length)
+    return `Summary spent: ${sum}\nthe most expensive item price is: ${max} 
+    the cheapest item price is: ${min} \nand the average spending is: ${average}`;
 }
 
-getSum();
-
-export {};
+console.log(app(expenses));
