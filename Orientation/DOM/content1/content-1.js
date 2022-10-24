290a4ee226a307bfcb1985ee2ee5fb1cdd838c15
Radovan Lipka <radovan.lipka@gmail.com>
2022-10-24 17:21:59 +0200

    elements content

--- Orientation/DOM/content1/content-1.js
+++ Orientation/DOM/content1/content-1.js
@@ -8,5 +8,3 @@ const allParagraphs = document.querySelectorAll('p');
 allParagraphs.forEach(function (paragraph) {
     paragraph.innerHTML = animalsParagraphHtml;
 });
-
-//pamätať na názvy premennych, googlit, skusat, ctrl+d na premenovanie viac riadkov premennej naraz, pamätať si na syntax for each
